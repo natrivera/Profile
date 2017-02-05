8cf4bc3cab96b53e0a91da0981adb853bc23dddf
Natividad Rivera <riveranatividad1@gmail.com>
2017-02-05 10:38:11 -0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,25 +1,23 @@
-  //run animations	 
-	    AOS.init();
+ 	 
+ //move picture on top	 
+$("#mask").click(function() {
+	if ($("#introduction").css("z-index") == "10") {
+		$("#introduction").css("z-index", "100");
+	} else {
+		$("#introduction").css("z-index", "10");
+	 }
+});	 
 		 
+//show the counter
+$(".bottomName").click(function() {
 		 
-	   //move picture on top	 
-	   	$("#mask").click(function() {
-	   	if ($("#introduction").css("z-index") == "10") {
-		    	$("#introduction").css("z-index", "100");
-		    }
-		    else {
-		    	$("#introduction").css("z-index", "10");
-		    }
-	   });	 
-		 
-		//show the counter
-		 $(".bottomName").click(function() {
-		 
-			 $("#counter").removeAttr("class");
-		 
-		 });
+	$("#counter").removeAttr("class");
+});
 
 $(document).ready(function() {
+	
+	//run animations	 
+	    AOS.init();
 
     //add animation to the form
     $(".conform").attr("data-aos","flip-up");
