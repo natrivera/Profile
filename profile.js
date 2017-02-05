4c00ab287cdb73b2dc3f54aab0a70d788b218ce5
Natividad Rivera <riveranatividad1@gmail.com>
2017-02-05 10:42:42 -0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,18 +1,6 @@
  	 
- //move picture on top	 
-$("#mask").click(function() {
-	if ($("#introduction").css("z-index") == "10") {
-		$("#introduction").css("z-index", "100");
-	} else {
-		$("#introduction").css("z-index", "10");
-	 }
-});	 
-		 
-//show the counter
-$(".bottomName").click(function() {
-		 
-	$("#counter").removeAttr("class");
-});
+	 
+
 
 $(document).ready(function() {
 	
@@ -23,4 +11,20 @@ $(document).ready(function() {
     $(".conform").attr("data-aos","flip-up");
     $(".conform").attr("data-aos-delay","800");
     $(".conform").attr("data-aos-duration","3000");
+	
+	
+	 //move picture on top
+	$("#mask").click(function() {
+	if ($("#introduction").css("z-index") == "10") {
+		$("#introduction").css("z-index", "100");
+	} else {
+		$("#introduction").css("z-index", "10");
+	 }
+	});	 
+		 
+	//show the counter
+	$(".bottomName").click(function() {
+		 
+		$("#counter").removeAttr("class");
+	});
 });
