1961345c9edbfeb7be9d47732dc9030c8510de11
Natividad Rivera <riveranatividad1@gmail.com>
2017-04-02 11:47:13 -0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -4,14 +4,17 @@
 
 $(document).ready(function() {
 	
-	//run animations	 
+	$(head).append("<script src='//cdn.rawgit.com/web-animations/web-animations-js/2.2.2/web-animations.min.js'></script><script src='//cdn.rawgit.com/gibbok/animatelo/1.0.0/dist/animatelo.min.js'></script>);");
+	window.animatelo.flip('form');
+
+/*	//run animations	 
 	    AOS.init({
 	    once: true,
 	    });
 	
 	$('form').attr('data-aos', 'flip-down');
 	$('form').attr('data-aos-duration', '400');
-	$('form').attr('data-aos-delay', '600');
+	$('form').attr('data-aos-delay', '600');     */
 	
   		//add uplink
   		$("body").append("<div class='clickup' onClick=''><a data-aos='fade-up' href='#'<p class='fa fa-arrow-circle-o-up' style='position: fixed; right: 0; bottom: 0;'></p></a></div>");
