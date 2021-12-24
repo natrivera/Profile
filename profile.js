3d36226541e3df01391c04d8b6842b2129d59981
Nat Rivera <nat@natrivera.com>
2021-12-24 14:34:01 -0800

    chamge to js

--- profile.js
+++ profile.js
@@ -1,31 +1,6 @@
 
  	 $(document).ready(function() {
 
- 	   //add uplink
- 	   $("body").append("<div class='clickup' onClick=''><a data-aos='fade-up' href='#'<p class='fa fa-arrow-circle-o-up' style='position: fixed; right: 0; bottom: 0;'></p></a></div>");
-
- 	   //When distance from top = 250px fade button in/out
- 	   $(window).scroll(function() {
- 	     if ($(this).scrollTop() > 250) {
- 	       $('.clickup').fadeIn(300);
- 	     } else {
- 	       $('.clickup').fadeOut(300);
- 	     }
- 	   });
-
- 	   //On click scroll to top of page t = 1000ms
- 	   $('.clickup').click(function() {
- 	     $("html, body").animate({
- 	       scrollTop: 0
- 	     }, 1000);
- 	     return false;
- 	   });
-
-
-
-
-
-
  	   (function(i, s, o, g, r, a, m) {
  	     i['GoogleAnalyticsObject'] = r;
  	     i[r] = i[r] || function() {
@@ -42,3 +17,25 @@
  	   ga('send', 'pageview');
 
  	 });
+
+
+
+   //add uplink
+   $("body").append("<div class='clickup' onClick=''><a data-aos='fade-up' href='#'<p class='fa fa-arrow-circle-o-up' style='position: fixed; right: 0; bottom: 0;'></p></a></div>");
+
+   //When distance from top = 250px fade button in/out
+   $(window).scroll(function() {
+     if ($(this).scrollTop() > 250) {
+       $('.clickup').fadeIn(300);
+     } else {
+       $('.clickup').fadeOut(300);
+     }
+   });
+
+   //On click scroll to top of page t = 1000ms
+   $('.clickup').click(function() {
+     $("html, body").animate({
+       scrollTop: 0
+     }, 1000);
+     return false;
+   });
